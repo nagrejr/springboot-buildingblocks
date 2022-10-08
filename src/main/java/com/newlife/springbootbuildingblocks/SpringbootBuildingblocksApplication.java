603551135b601b1b09bcab2f9a13310abe9200a2
surajnagre <surajnagre@localhost>
2022-10-09 00:28:34 +0530

    second commit

--- src/main/java/com/newlife/springbootbuildingblocks/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/newlife/springbootbuildingblocks/SpringbootBuildingblocksApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
+	//second commit
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
