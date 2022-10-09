eb109b4e59a9a86375ad0a0afad36ad8b4111346
surajnagre <surajnagre@localhost>
2022-10-09 19:27:00 +0530

    ResponseStatusException first commit

--- src/main/java/com/newlife/springbootbuildingblocks/controllers/UserController.java
+++ src/main/java/com/newlife/springbootbuildingblocks/controllers/UserController.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,52 +14,88 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.newlife.springbootbuildingblocks.entities.User;
+import com.newlife.springbootbuildingblocks.exceptions.UserAlreadyExistException;
+import com.newlife.springbootbuildingblocks.exceptions.UserNotFoundException;
 import com.newlife.springbootbuildingblocks.services.UserService;
 
 @RestController
 public class UserController {
-	
-	//autowired the service
+
+	// autowired the service
 	@Autowired
 	private UserService userService;
 
-	//getallusers method from service class
+	// getallusers method from service class
 	@GetMapping("/users")
-	public List<User> getAllUsers(){
+	public List<User> getAllUsers() {
 		return userService.getAllUsers();
 	}
-	
-	//postmapping for creating user
-	//accept User input as a requestbody
+
+	// postmapping for creating user
+	// accept User input as a requestbody
 	@PostMapping("users")
-	public User createUser(@RequestBody User user) {
-		return userService.createUser(user);
+	// UriComponentsBuilder - factory class for getting instances of UriComponents which are helpful for constructing URIs
+	public ResponseEntity<Void> createUser(@RequestBody User user, UriComponentsBuilder uriComponentsBuilder) {
+		
+		// user created 201 status code
+		// implementation is only in controller, not in service
+		try {
+			userService.createUser(user);
+			// http headers impl.
+			HttpHeaders httpHeaders = new HttpHeaders();
+			httpHeaders.setLocation(uriComponentsBuilder.path("/users/{id}").buildAndExpand(user.getId()).toUri());
+			return new ResponseEntity<Void>(httpHeaders, HttpStatus.CREATED);
+		}
+		catch(UserAlreadyExistException e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+		}
 	}
-	
-	//getuserbyid method
+
+	// getuserbyid method
 	@GetMapping("/users/{id}")
-	//path variable is to use id from mapping for input
-	public Optional<User> getUserById(@PathVariable("id") Long id){
-		return userService.getUserById(id);
+	// path variable is to use id from mapping for input
+	public Optional<User> getUserById(@PathVariable("id") Long id) {
+
+		// try catch block to handle UserNotException
+		try {
+			return userService.getUserById(id);
+		}
+		// send HTTP status response of user not found
+		catch (UserNotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+		}
+
 	}
-	
+
 	@PutMapping("/users/{id}")
-	//using pathvariable and requestbody as a input
+	// using pathvariable and requestbody as a input
 	public User updateUserById(@PathVariable("id") Long id, @RequestBody User user) {
-		return userService.updateuserById(id, user);
+
+		// try catch block to handle UserNotException
+		try {
+			return userService.updateuserById(id, user);
+		}
+		// send HTTP status response of user not found
+		catch (UserNotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+		}
+
 	}
-	
+
 	@DeleteMapping("/users/{id}")
-	//taking only id as a pathvariable input
+	// taking only id as a pathvariable input
 	public void deleteUserById(@PathVariable("id") Long id) {
 		userService.deleteUserById(id);
 	}
-	
-	//using byusername in mapping due to
-	//if used directly username it will act as a id which is mentioned above for getmapping
-	//used username as a pathvariable input
+
+	// using byusername in mapping due to
+	// if used directly username it will act as a id which is mentioned above for
+	// getmapping
+	// used username as a pathvariable input
 	@GetMapping("users/byusername/{username}")
 	public User getUserByUsername(@PathVariable("username") String username) {
 		return userService.getUserByUsername(username);
--- src/main/java/com/newlife/springbootbuildingblocks/services/UserService.java
+++ src/main/java/com/newlife/springbootbuildingblocks/services/UserService.java
@@ -4,53 +4,88 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.newlife.springbootbuildingblocks.entities.User;
+import com.newlife.springbootbuildingblocks.exceptions.UserAlreadyExistException;
+import com.newlife.springbootbuildingblocks.exceptions.UserNotFoundException;
 import com.newlife.springbootbuildingblocks.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
-	//autowird to repo
+
+	// autowird to repo
 	@Autowired
 	private UserRepository userRepository;
-	
-	//getallusers method 
-	public List<User> getAllUsers(){
+
+	// getallusers method
+	public List<User> getAllUsers() {
 		return userRepository.findAll();
 	}
-	
-	//createuser method with return as a User
-	//will take input as a User
-	public User createUser(User user) {
+
+	// createuser method with return as a User
+	// will take input as a User
+	// to check user already exists or not - UserAlreadyExistException
+	public User createUser(User user) throws UserAlreadyExistException{
+		
+		// user is already existed in repository or not
+		User existingUser = userRepository.findUserByUsername(user.getUsername());
+		
+		// if existingUser is present in repo. 
+		if(existingUser != null) {
+			throw new UserAlreadyExistException("User is already exist, please try another username");
+		}
 		return userRepository.save(user);
 	}
-	
-	//getuserbyid method
-	//optional data type may or may not have the null value
-	//using optional data type due to check whether the id is present or not present
-	public Optional<User> getUserById(Long id) {
+
+	// getuserbyid method
+	// optional data type may or may not have the null value
+	// using optional data type due to check whether the id is present or not
+	// present
+	// throws UserNotFoundException
+	public Optional<User> getUserById(Long id) throws UserNotFoundException {
 		Optional<User> user = userRepository.findById(id);
+
+		// when id is not present it will throw an exception message
+		if (!user.isPresent()) {
+			throw new UserNotFoundException("User not found in the repository");
+		}
 		return user;
 	}
-	
-	//taking id as input to search the id exist or not
-	//if exists then use the user object to update 
-	public User updateuserById(Long id, User user) {
+
+	// taking id as input to search the id exist or not
+	// if exists then use the user object to update
+	public User updateuserById(Long id, User user) throws UserNotFoundException {
+
+		Optional<User> optionalUser = userRepository.findById(id);
+
+		// when id is not present it will throw an exception message
+		if (!optionalUser.isPresent()) {
+			throw new UserNotFoundException("User not found in the repository, provide correct id");
+		}
 		user.setId(id);
 		return userRepository.save(user);
 	}
-	
-	//not returning any data so return type is void
-	//first have to check whether the id is present or not
+
+	// not returning any data so return type is void
+	// first have to check whether the id is present or not
 	public void deleteUserById(Long id) {
-		if(userRepository.findById(id).isPresent()) {
-			userRepository.deleteById(id);
+
+		// no need for throws UserNotFoundException in the method definition
+		Optional<User> optionalUser = userRepository.findById(id);
+
+		// when id is not present it will throw an exception message
+		// no need for ResponseStatusException in controller class
+		if (!optionalUser.isPresent()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"User not found in the repository, provide correct id");
 		}
+		
+		userRepository.deleteById(id);
 	}
-	
-	//using string username as a input
+
+	// using string username as a input
 	public User getUserByUsername(String username) {
 		return userRepository.findUserByUsername(username);
 	}
